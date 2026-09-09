0ef8a866b9f6675a5b8b22f2ae4212090d7ff08d
Ashish Sapkota <ashishspkt6566@gmail.com>
2026-09-09 17:54:59 +0000

    Implement basic VGA text output in the _start function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 #![no_std]
 #![no_main]
 
+use core::panic::PanicInfo;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
-use core::panic::PanicInfo;
+static HELLO: &[u8] = b"Hello Aacs!";
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
