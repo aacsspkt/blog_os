aa3321e4de6c4179dc0346e05e360012b5366133
Ashish Sapkota <ashishspkt6566@gmail.com>
2026-09-16 15:24:20 +0000

    Refactor main function to include test framework and improve panic handling

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-
+use blog_os::println;
 use core::panic::PanicInfo;
-
-static HELLO: &str = "Hello Aacs!";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("{}", HELLO);
-    panic!("Some panic message");
-    // loop {}
+    println!("Hello World{}", "!");
+
+    blog_os::init();
+
+    x86_64::instructions::interrupts::int3();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    loop {}
 }
 
+/// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
 }
