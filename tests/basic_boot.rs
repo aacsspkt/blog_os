102c5ff3c4bbbfd766d858c972c0c45cfeee9324
Ashish Sapkota <ashishspkt6566@gmail.com>
2026-09-16 15:23:36 +0000

    Add basic boot and panic tests for the blog_os project

---
+++ tests/basic_boot.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+use blog_os::println;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
